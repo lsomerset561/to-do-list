5eabaa7e17d19bc1f17fe27853e6d5eed36b8f3a
Lindsey Somerset <somerset.lindsey@gmail.com>
2016-03-17 15:34:58 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * 1. When the add-new button is clicked
+ *    a. Check to see if the input field has text
+ *      i. if true
+ *        Create a new li with the users text
+ *        Add it to the top of the list
+ *        Clear input filed
+ *      ii. if false
+ *        Alert the user, 'Step 1 : Fill out text'
+ *
+ * 2. When a user clicks the delete button it should delete that li, with a prompt
+ * 3. When the user has deleted all the tasks, congratulate them in some way
+ *
+ * ******** BONUS ********
+ *
+ * 4. When user deletes to do, animate the height to 0.
+ * 5. Figure out a way for the user to edit the text.
+ * 6. Save tasks in local storage for persistence.
+ *
+ */
+
+var $newTodoInput = $('.new-todo-input');
+var $list = $('.list');
+var currentMax = 6;
+
+$(function(){
+
+});
